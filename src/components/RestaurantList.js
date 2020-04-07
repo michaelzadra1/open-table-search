12a558e46f7a2ebcb9f32f3da29ff30e75019c13
Michael Zadra <michaelzadra1@gmail.com>
2020-04-07 17:47:09 -0400

    Adds error handling to RestaurantList component

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Container, Grid, Typography } from '@material-ui/core';
+import { connect } from 'react-redux';
+import { isEmpty } from 'lodash';
+import {
+	Container,
+	Grid,
+	Typography,
+	CircularProgress,
+	Box,
+	FormHelperText
+} from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core/styles';
-
 import RestaurantCard from './RestaurantCard';
-
-const restaurants = [
-	{
-		id: 21307,
-		name: 'Scaramouche Restaurant',
-		address: '1 Benvenuto Place',
-		city: 'Toronto',
-		state: 'ON',
-		area: 'Toronto / SW Ontario',
-		postal_code: 'M4V 2L1',
-		country: 'CA',
-		phone: '4169618011',
-		lat: 43.68207,
-		lng: -79.40041,
-		price: 4,
-		reserve_url: 'http://www.opentable.com/single.aspx?rid=21307',
-		mobile_reserve_url: 'http://mobile.opentable.com/opentable/?restId=21307',
-		image_url: 'https://www.opentable.com/img/restimages/21307.jpg'
-	}
-];
 
 const theme = createMuiTheme();
 
@@ -36,22 +24,43 @@ const ResturantGrid = styled(Grid)`
 	list-style: none;
 `;
 
-const RestaurantList = () => {
-	const renderList = (restaurants) => {
+const RestaurantList = (props) => {
+	const renderLoadingCircle = () => (
+		<CircularProgress color="inherit" data-testid="loading-restaurants" />
+	);
+
+	const renderEmptyResults = () => (
+		<Typography component="h2" variant="h6" color="textSecondary">
+			Empty search results. Try again with a new query.
+		</Typography>
+	);
+
+	const renderError = () => (
+		<FormHelperText role="error" error={true}>
+			{props.restaurants.error}
+		</FormHelperText>
+	);
+
+	const renderList = () => {
+		const { totalEntries, restaurants } = props.restaurants;
+		const { searchTouched } = props.search;
+
 		return (
 			<ResturantGrid container spacing={4} component="ul">
-				{restaurants.map((restaurant) => (
-					<Grid
-						item
-						key={restaurant.id}
-						component="li"
-						xs={12}
-						sm={6}
-						md={4}
-					>
-						<RestaurantCard {...restaurant}></RestaurantCard>
-					</Grid>
-				))}
+				{totalEntries > 0
+					? restaurants.map((restaurant) => (
+							<Grid
+								item
+								key={restaurant.id}
+								component="li"
+								xs={12}
+								sm={6}
+								md={4}
+							>
+								<RestaurantCard {...restaurant}></RestaurantCard>
+							</Grid>
+					  ))
+					: searchTouched && renderEmptyResults()}
 			</ResturantGrid>
 		);
 	};
@@ -62,10 +71,21 @@ const RestaurantList = () => {
 				Restaurant search results
 			</Typography>
 			<RestaurantListContainer maxWidth="md">
-				{renderList(restaurants)}
+				<Box width={'100%'} display="flex" justifyContent="center">
+					{props.restaurants.loading
+						? renderLoadingCircle()
+						: isEmpty(props.restaurants.error)
+						? renderList()
+						: renderError()}
+				</Box>
 			</RestaurantListContainer>
 		</React.Fragment>
 	);
 };
 
-export default RestaurantList;
+const mapStateToProps = (state) => ({
+	restaurants: state.restaurants,
+	search: state.search
+});
+
+export default connect(mapStateToProps)(RestaurantList);
