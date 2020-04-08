844f9ce841a6e1e12bd9505fc9be1871db7283b8
Michael Zadra <michaelzadra1@gmail.com>
2020-04-08 00:27:36 -0400

    Tweaks base url for OpenTable to use https instead of http

--- src/apis/openTable.js
+++ src/apis/openTable.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const OPEN_TABLE_OPEN_URL = 'http://opentable.herokuapp.com/api';
+const OPEN_TABLE_OPEN_URL = 'https://opentable.herokuapp.com/api';
 
 export default axios.create({
 	baseURL: OPEN_TABLE_OPEN_URL
