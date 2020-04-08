968f196ff27f35fedcfae6636bb83689377ed10b
Michael Zadra <michaelzadra1@gmail.com>
2020-04-08 01:04:21 -0400

    Moves mock restaurants list to mock file

--- src/tests/RefineSearch.spec.js
+++ src/tests/RefineSearch.spec.js
@@ -5,43 +5,7 @@ import userEvent from '@testing-library/user-event';
 
 import RefineSearch from '../components/RefineSearch';
 import { renderWithRedux } from './testHelpers';
-
-const restaurants = [
-	{
-		id: 21307,
-		name: 'Beer Restaurant',
-		address: '1 Benvenuto Place',
-		city: 'Toronto',
-		state: 'ON',
-		area: 'Toronto / SW Ontario',
-		postal_code: 'M4V 2L1',
-		country: 'CA',
-		phone: '4169618011',
-		lat: 43.68207,
-		lng: -79.40041,
-		price: 4,
-		reserve_url: 'http://www.opentable.com/single.aspx?rid=21307',
-		mobile_reserve_url: 'http://mobile.opentable.com/opentable/?restId=21307',
-		image_url: 'https://www.opentable.com/img/restimages/21307.jpg'
-	},
-	{
-		id: 82957,
-		name: "The Sultan's Tent",
-		address: '49 Front St E',
-		city: 'Toronto',
-		state: 'ON',
-		area: 'Toronto / SW Ontario',
-		postal_code: 'M5E 1B3',
-		country: 'CA',
-		phone: '4169610601x',
-		lat: 43.648033,
-		lng: -79.374377,
-		price: 4,
-		reserve_url: 'http://www.opentable.com/single.aspx?rid=82957',
-		mobile_reserve_url: 'http://mobile.opentable.com/opentable/?restId=82957',
-		image_url: 'https://www.opentable.com/img/restimages/82957.jpg'
-	}
-];
+import { shortRestaurantsListMock } from '../__mocks__/mock';
 
 describe('<SearchInput />', () => {
 	beforeEach(() => {
@@ -107,7 +71,7 @@ describe('<SearchInput />', () => {
 			params: { city: 'Toronto', name: 'beer', page: 1 }
 		});
 		await mockAxios.get.mockResolvedValueOnce({
-			data: { restaurants, total_entries: 2, page: 1 }
+			data: { shortRestaurantsListMock, total_entries: 2, page: 1 }
 		});
 		expect(mockAxios.get).toHaveBeenCalledWith('/restaurants', {
 			params: { city: 'Toronto', address: 'beer', page: 1 }
@@ -115,7 +79,7 @@ describe('<SearchInput />', () => {
 		// Searches by address
 		expect(mockAxios.get).toHaveBeenCalledTimes(2);
 		await mockAxios.get.mockResolvedValueOnce({
-			data: { restaurants, total_entries: 2, page: 1 }
+			data: { shortRestaurantsListMock, total_entries: 2, page: 1 }
 		});
 	});
 	it('performs search with refined query on click of refine search button', async () => {
@@ -138,7 +102,7 @@ describe('<SearchInput />', () => {
 			params: { city: 'Toronto', name: 'beer', page: 1 }
 		});
 		await mockAxios.get.mockResolvedValueOnce({
-			data: { restaurants, total_entries: 2, page: 1 }
+			data: { shortRestaurantsListMock, total_entries: 2, page: 1 }
 		});
 	});
 	it('performs search with empty refined query param', async () => {
@@ -159,7 +123,7 @@ describe('<SearchInput />', () => {
 			params: { city: 'Toronto', page: 1 }
 		});
 		await mockAxios.get.mockResolvedValueOnce({
-			data: { restaurants, total_entries: 2, page: 1 }
+			data: { shortRestaurantsListMock, total_entries: 2, page: 1 }
 		});
 	});
 });
